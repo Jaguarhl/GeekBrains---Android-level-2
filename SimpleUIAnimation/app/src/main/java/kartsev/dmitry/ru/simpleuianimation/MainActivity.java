d9776400b478114fe4afa67964ca110f0d8f680e
Jaguarhl <dek.alpha@gmail.com>
2017-01-16 02:12:24 +0300

    Simple custom View with animation.

---
+++ SimpleUIAnimation/app/src/main/java/kartsev/dmitry/ru/simpleuianimation/MainActivity.java
@@ -0,0 +1,85 @@
+package kartsev.dmitry.ru.simpleuianimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import kartsev.dmitry.ru.simpleuianimation.views.SimpleShapeView;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private Button btnTriangle, btnCircle, btnSquare, btnRotate, btnScale, btnAlpha;
+    private SimpleShapeView shape;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initViews();
+        setButtonsBehavior();
+    }
+
+    private void initViews() {
+        btnCircle = (Button) findViewById(R.id.buttonCircle);
+        btnSquare = (Button) findViewById(R.id.buttonSquare);
+        btnTriangle = (Button) findViewById(R.id.buttonTriangle);
+        btnRotate = (Button) findViewById(R.id.buttonRotate);
+        btnScale = (Button) findViewById(R.id.buttonScale);
+        btnAlpha = (Button) findViewById(R.id.buttonAlpha);
+        shape = (SimpleShapeView) findViewById(R.id.shapeView);
+    }
+
+    private void setButtonsBehavior() {
+        final Animation animationRotateCenter = AnimationUtils.loadAnimation(
+                this, R.anim.rotate);
+        final Animation animationScale = AnimationUtils.loadAnimation(this, R.anim.scale);
+        final Animation animationAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
+
+        btnSquare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shape.setShapeType(SimpleShapeView.SHAPE_SQUARE);
+            }
+        });
+
+        btnTriangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shape.setShapeType(SimpleShapeView.SHAPE_TRIANGLE);
+            }
+        });
+
+        btnCircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shape.setShapeType(SimpleShapeView.SHAPE_CIRCLE);
+            }
+        });
+
+        btnRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shape.startAnimation(animationRotateCenter);
+
+            }
+        });
+
+        btnScale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shape.startAnimation(animationScale);
+            }
+        });
+
+        btnAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shape.startAnimation(animationAlpha);
+            }
+        });
+    }
+}
---
+++ SimpleUIAnimation/app/src/main/java/kartsev/dmitry/ru/simpleuianimation/views/SimpleShapeView.java
@@ -0,0 +1,127 @@
+package kartsev.dmitry.ru.simpleuianimation.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.View;
+
+import kartsev.dmitry.ru.simpleuianimation.R;
+
+/**
+ * Created by Jag on 15.01.2017.
+ */
+
+public class SimpleShapeView extends View {
+    public static final String SHAPE_TRIANGLE = "triangle";
+    public static final String SHAPE_CIRCLE = "circle";
+    public static final String SHAPE_SQUARE = "square";
+    public static final String ANIMATION_ROTATION = "rotation";
+    public static final int OFFSET = 20;
+    private Paint mPaint = null;
+    private String shapeType = null;
+    private int shapeSize = 0;
+    private String shapeAnimation = null;
+    private int backgroundColor = 0;
+
+    public SimpleShapeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SimpleShapeView);
+        /*switch(a.getString(R.styleable.SimpleShapeView_shape_type)) {
+            case SHAPE_TRIANGLE:
+                this.shapeType = 1;
+                break;
+            case SHAPE_CIRCLE:
+                this.shapeType = 2;
+                break;
+            case SHAPE_SQUARE:
+                this.shapeType = 3;
+                break;
+            default:
+                this.shapeType = -1;
+                break;
+         }*/
+         this.shapeType = a.getString(R.styleable.SimpleShapeView_shape_type);
+         this.shapeSize = a.getInt(R.styleable.SimpleShapeView_shape_size, 0);
+        if(this.shapeSize > this.getHeight()) {
+            this.shapeSize /= 2;
+        }
+         this.shapeAnimation = a.getString(R.styleable.SimpleShapeView_shape_animation);
+
+         /*switch(a.getString(R.styleable.SimpleShapeView_shape_animation)) {
+             case ANIMATION_ROTATION:
+                 this.shapeAnimation = 1;
+                 break;
+             default:
+                 this.shapeAnimation = -1;
+                 break;
+         }*/
+
+         mPaint = new Paint();
+         mPaint.setColor(a.getColor(R.styleable.SimpleShapeView_shape_color, 0));
+         mPaint.setAntiAlias(true);
+         backgroundColor = a.getColor(R.styleable.SimpleShapeView_shape_background_color, 0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int x = this.getWidth() / 2;
+        int y = this.getHeight() / 2;
+
+        if((!shapeType.isEmpty()) && (backgroundColor != 0)) {
+            canvas.drawColor(backgroundColor);
+
+            switch (shapeType) {
+                case SHAPE_CIRCLE:
+                    canvas.drawCircle(x, y, shapeSize / 2, mPaint);
+                    break;
+                case SHAPE_TRIANGLE:
+                    drawTriangle(x - (shapeSize / 2), y + (shapeSize / 2), shapeSize,
+                            shapeSize, false, mPaint, canvas);
+                    break;
+                case SHAPE_SQUARE:
+                    canvas.drawRect(x - (shapeSize / 2), y - (shapeSize / 2), x + (shapeSize / 2),
+                            y + (shapeSize / 2), mPaint);
+                    break;
+            }
+        }
+    }
+
+    private void drawTriangle(int x, int y, int width, int height, boolean inverted, Paint paint, Canvas canvas){
+
+        Point p1 = new Point(x,y);
+        int pointX = x + width/2;
+        int pointY = inverted?  y + height : y - height;
+
+        Point p2 = new Point(pointX,pointY);
+        Point p3 = new Point(x+width,y);
+
+
+        Path path = new Path();
+        path.setFillType(Path.FillType.EVEN_ODD);
+        path.moveTo(p1.x,p1.y);
+        path.lineTo(p2.x,p2.y);
+        path.lineTo(p3.x,p3.y);
+        path.close();
+
+        canvas.drawPath(path, paint);
+    }
+
+    public boolean setShapeType(String type) {
+        type = type.toLowerCase();
+        if((type.equals(SHAPE_CIRCLE)) || (type.equals(SHAPE_SQUARE)) || (type.equals(SHAPE_TRIANGLE))) {
+            this.shapeType = type;
+            this.invalidate();
+            return true;
+        }
+
+        return false;
+    }
+}
